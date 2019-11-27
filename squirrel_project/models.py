209c8ca3595066145c3c965026df95b216b5e4e4
Yvonne Lin <ll3367@columbia.edu>
2019-11-27 00:10:03 +0000

    Create Squirrel Model

--- squirrel_project/models.py
+++ squirrel_project/models.py
@@ -1,40 +1,104 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 class Squirrel(models.Model):
-    X = models.DecimalField(
-            max_digits=20, 
-            deciman_places=18,
+    x = models.DecimalField(
+            max_digits=20,
+            decimal_places=18,
             help_text=_('Longitude coordinate for squirrel sighting point'),
             )
-    Y = models.DecimalField(
-            max_digits=20, 
-            deciman_places=18,
+    y = models.DecimalField(
+            max_digits=20,
+            decimal_places=18,
             help_text=_('Latitude coordinate for squirrel sighting point'),
             )   
     
-    Unique_Squirrel_ID = models.CharField(
+    unique_squirrel_id = models.CharField(
             max_length=20,
             help_text=_('Identification tag for each squirrel sightings'),
             )
     
-    Hectare = models.CharField(max_length=3)
+    hectare = models.CharField(
+            max_length=3,
+            )
     
-    Shift = models.CharField(max_length=2)
+    shift = models.CharField(
+            max_length=2,
+            )
     
-    Data = models.DateField(
+    date = models.DateField(
             help_text=_('Sighting session day and month'),
             )
     
-    Hectare_Squirrel_Number = models.IntegerField()
+    hectare_squirrel_number = models.IntegerField()
     
-    Age = models.CharField(
+    age = models.CharField(
             max_length=8,
             )
     
-    Primary_Fur_Color = models.CharField(
+    primary_fur_color = models.CharField(
             max_length=8,
             )
             
-    Highlight_Fur_Color = models.CharField(
+    highlight_fur_color = models.CharField(
             max_length=8,
             )
+
+    combination_of_primary_and = models.CharField(
+            max_length=100,
+            )
+
+    color_notes= models.CharField(
+            max_length=100,
+            )
+
+    location = models.CharField(
+            max_length=100,
+            )
+
+    above_ground_sighter = models.CharField(
+            max_length=100,
+            )
+
+    specific_location = models.CharField(
+            max_length=100,
+            )
+
+    running = models.BooleanField()
+    
+    chasing = models.BooleanField()
+
+    climbing = models.BooleanField()
+
+    eating = models.BooleanField()
+
+    foraging = models.BooleanField()
+
+    other_activities = models.CharField(
+            max_length=200,
+            )
+
+    kuks = models.BooleanField()
+
+    quaas = models.BooleanField()
+
+    moans = models.BooleanField()
+
+    tail_flags = models.BooleanField()
+
+    tail_twitches = models.BooleanField()
+
+    approaches = models.BooleanField()
+
+    indifferent = models.BooleanField()
+
+    runs_from = models.BooleanField()
+
+    other_interactions = models.CharField(
+            max_length=200,
+            )
+    
+    geocoded_column = models.CharField(
+            max_length=500,
+            )
+
