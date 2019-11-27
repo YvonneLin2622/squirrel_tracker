5d9d3025b751fb3885e844dbe69b41f08ee07994
Yvonne Lin <ll3367@columbia.edu>
2019-11-27 02:04:11 +0000

    Modified Export Squirrel Data Command File

--- squirrel_project/management/commands/export_squirrel_data.py
+++ squirrel_project/management/commands/export_squirrel_data.py
@@ -1,8 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from squirrel_project.models import Squirrel
+import csv
+import sys
+
 class Command(BaseCommand):
     help = 'Export Squirrel Data in CSV format'
 
     def add_arguments(self, parser):
-        parser.add_argument('file path', type=str, nargs='+', help='Indicates file path.')
+        parser.add_argument('args', type=str, nargs='+', help='Indicates file path.')
 
     def handle(self, *args. **kwargs):
+        filepath = args[0]
+        fields = Squirrel._meta.fields
+        with open(filepath, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            for obj in Squirrel.ojects.all():
+                row = []
+                for field in fields:
+                    row.append(getattr(obj, field.name))
+                writer.writerow(row)
+
+
 
