5710cc41bc12f85befb3a59491bca0bc058ed032
Yvonne Lin <ll3367@columbia.edu>
2019-11-27 01:43:42 +0000

    Successfully import squirrel data

--- squirrel_project/management/commands/import_squirrel_data.py
+++ squirrel_project/management/commands/import_squirrel_data.py
@@ -8,9 +8,25 @@ class Command(BaseCommand):
     help = 'Import Squirrel Data. File path should be passed.'
     
     def add_arguments(self, parser):
-        parser.add_argument('file path', nargs='*', type = str, help='Indicates the file path')
+        parser.add_argument('args', nargs='*', type = str, help='Indicates the file path')
     
     def handle(self, *args, **kwargs):
         filepath = args[0]
+        with open(filepath) as squirrel_data:
+            reader = csv.reader(squirrel_data)
+            header = squirrel_data.readline().strip().split(',')
+            fields = [field.lower().replace(' ','_').replace('/','_') for field in header]
+            for row in reader:
+                obj = Squirrel()
+                for col_num, value in enumerate(row):
+                    if value == 'true':
+                        value  = True
+                    if value == 'false':
+                        value = False
+                    if col_num == 5:
+                        value = value[4:]+'-'+value[:2]+'-'+value[2:4]
+                    
+                    setattr(obj, fields[col_num], value)
+                obj.save()
 
 
