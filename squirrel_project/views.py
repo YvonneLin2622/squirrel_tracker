e522c76caac6071dbaf58b4bf5f68a1525795ccb
Ziye Meng <zm2335@columbia.edu>
2019-12-03 18:26:03 +0000

    Add list function

--- squirrel_project/views.py
+++ squirrel_project/views.py
@@ -4,3 +4,9 @@ from .models import Squirrel
 def map(request):
     squirrelList=Squirrel.objects.all()
     return render(request, 'squirrel_project/map.html', locals())
+
+def list(request):
+    squirrelList=Squirrel.objects.all()
+    return render(request, 'squirrel_project/list.html', locals())
+
+
