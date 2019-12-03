5b1cb278e5260c36dcf11c81df0566c4c240cd09
Yvonne Lin <ll3367@columbia.edu>
2019-12-03 23:51:57 +0000

    Modified Squirrel model

--- squirrel/adminll.py
+++ squirrel/adminll.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from django.contrib import admin
-
 from .models import Squirrel
 
-admin.site.register(Squirrel)
+adminll.site.register(Squirrel)
--- squirrel_project/models.py
+++ squirrel_project/models.py
@@ -6,100 +6,107 @@ class Squirrel(models.Model):
             max_digits=20,
             decimal_places=18,
             help_text=_('Longitude coordinate for squirrel sighting point'),
+            blank=True, null=True,
             )
     y = models.DecimalField(
             max_digits=20,
             decimal_places=18,
             help_text=_('Latitude coordinate for squirrel sighting point'),
+            blank=True, null=True,
             )   
     
     unique_squirrel_id = models.CharField(
             max_length=20,
             help_text=_('Identification tag for each squirrel sightings'),
+            blank=True, null=True,
             )
     
     hectare = models.CharField(
             max_length=3,
+            blank=True, null=True,
             )
     
     shift = models.CharField(
-            max_length=2,
+            max_length=2,blank=True, null=True,
             )
     
     date = models.DateField(
             help_text=_('Sighting session day and month'),
+            blank=True, null=True,
             )
     
-    hectare_squirrel_number = models.IntegerField()
+    
+    hectare_squirrel_number = models.IntegerField(blank=True, null=True,)
     
     age = models.CharField(
             max_length=8,
+            blank=True, null=True,
             )
     
     primary_fur_color = models.CharField(
-            max_length=8,
+            max_length=8,blank=True, null=True,
             )
             
     highlight_fur_color = models.CharField(
-            max_length=8,
+            max_length=8,blank=True, null=True,
             )
 
     combination_of_primary_and_highlight_color = models.CharField(
-            max_length=100,
+            max_length=100,blank=True, null=True,
             )
 
     color_notes= models.CharField(
-            max_length=100,
+            max_length=100,blank=True, null=True,
             )
 
     location = models.CharField(
-            max_length=100,
+            max_length=100,blank=True, null=True,
             )
 
     above_ground_sighter_measurement = models.CharField(
-            max_length=100,
+            max_length=100,blank=True, null=True,
             )
 
     specific_location = models.CharField(
-            max_length=100,
+            max_length=100,blank=True, null=True,
             )
 
-    running = models.BooleanField()
+    running = models.BooleanField(blank=True, null=True,)
     
-    chasing = models.BooleanField()
+    chasing = models.BooleanField(blank=True, null=True,)
 
-    climbing = models.BooleanField()
+    climbing = models.BooleanField(blank=True, null=True,)
 
-    eating = models.BooleanField()
+    eating = models.BooleanField(blank=True, null=True,)
 
-    foraging = models.BooleanField()
+    foraging = models.BooleanField(blank=True, null=True,)
 
     other_activities = models.CharField(
-            max_length=200,
+            max_length=200,blank=True, null=True,
             )
 
-    kuks = models.BooleanField()
+    kuks = models.BooleanField(blank=True, null=True,)
 
-    quaas = models.BooleanField()
+    quaas = models.BooleanField(blank=True, null=True,)
 
-    moans = models.BooleanField()
+    moans = models.BooleanField(blank=True, null=True,)
 
-    tail_flags = models.BooleanField()
+    tail_flags = models.BooleanField(blank=True, null=True,)
 
-    tail_twitches = models.BooleanField()
+    tail_twitches = models.BooleanField(blank=True, null=True,)
 
-    approaches = models.BooleanField()
+    approaches = models.BooleanField(blank=True, null=True,)
 
-    indifferent = models.BooleanField()
+    indifferent = models.BooleanField(blank=True, null=True,)
 
-    runs_from = models.BooleanField()
+    runs_from = models.BooleanField(blank=True, null=True,)
 
     other_interactions = models.CharField(
-            max_length=200,
+            max_length=200,blank=True, null=True,
             )
     
     lat_long = models.CharField(
-            max_length=500,
+            max_length=500,blank=True, null=True,
             )
     zip_codes = models.CharField(
             max_length=5,
@@ -110,3 +117,5 @@ class Squirrel(models.Model):
     city_council_districts = models.IntegerField(blank=True, null=True)
     police_precincts = models.IntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return self.unique_squirrel_id
