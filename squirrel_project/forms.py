f6699eff1a3426d3f147c8f9b76d1c7daf5d4581
Ziye Meng <zm2335@columbia.edu>
2019-12-06 02:58:31 +0000

    modify forms.py

--- squirrel_project/forms.py
+++ squirrel_project/forms.py
@@ -15,5 +15,5 @@ class SquirrelForm(ModelForm):
 class EditSquirrelForm(ModelForm):
     class Meta:
         model = Squirrel
-        fields = ('x','y','shift',"age","primary_fur_color","location","specific_location","running","chasing","climbing","eating","foraging","other_activities","kuks","quaas","tail_flags","tail_twitches","approaches","indifferent","runs_from" )
+        fields = ('x','y','shift',"age","primary_fur_color","location","specific_location","running","date","chasing","climbing","eating","foraging","other_activities","kuks","quaas","tail_flags","tail_twitches","approaches","indifferent","runs_from" )
 
