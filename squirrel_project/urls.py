7adfb31c2f8b1ce494d8b38ccb8b3330687a936f
Ziye Meng <zm2335@columbia.edu>
2019-12-08 20:36:14 +0000

    Merge urls.py

--- squirrel_project/urls.py
+++ squirrel_project/urls.py
@@ -4,10 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.list),
-<<<<<<< HEAD
     path("add", views.add),
     path("<unique_squirrel_id>/", views.edit),
-=======
     path('stats/', views.squirrel_stats),
->>>>>>> 82b12596bdf6d7c2207a78c59ead0cc530f829b1
 ]
