9f044216f6be2cb8a82eb63989b0adf06c096813
ZiyeMeng96 <56941112+ZiyeMeng96@users.noreply.github.com>
2019-12-08 17:46:04 -0500

    Update forms.py

--- squirrel_project/forms.py
+++ squirrel_project/forms.py
@@ -5,7 +5,4 @@ from django.utils.translation import gettext as _
 class SquirrelForm(ModelForm):
     class Meta:
         model = Squirrel
-        fields = ('x','y','unique_squirrel_id','shift',"age","primary_fur_color","location","
-specific_location","running",'date',"chasing","climbing","eating","foraging","other_activitie
-s","kuks","quaas","moans","tail_flags","tail_twitches","approaches","indifferent","runs_from"
-)
+        fields = ('x','y','unique_squirrel_id','shift',"age","primary_fur_color","location","specific_location","running",'date',"chasing","climbing","eating","foraging","other_activities","kuks","quaas","moans","tail_flags","tail_twitches","approaches","indifferent","runs_from")
